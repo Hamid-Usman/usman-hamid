690e6b4121bc08464cb7492a075bebb5440ea0c2
Hamid-Usman <abdulhamidusman218@gmail.com>
2025-06-29 03:14:47 +0100

    add animation effects to Experience component; implement WaterDropGrid and slideUp variant

--- motion/variants/slideStaggered.ts
+++ motion/variants/slideStaggered.ts
@@ -1,5 +1,5 @@
 export const slideStaggered = (delay: number = 0) => ({
-    initial: {x: -200, opacity: 1},
+    initial: {x: -200, opacity: 0},
     animate: {x: 0, opacity: 1, transition: { duration: 0.8, delay }},
     exit: {x: -200, opacity: 0, transition: { duration: 0.5}} 
 });
---
+++ motion/variants/slideUp.ts
@@ -0,0 +1,6 @@
+export const  slideUp =  {
+    initial: { y: -100, opacity: 0 },
+    animate: { y: 0, opacity: 1, transition: { duration: 1.07 }},
+    exit: { y: -100, opacity: 0 },
+    
+}
