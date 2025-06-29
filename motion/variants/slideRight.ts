a73a01ca64904f503380334d709d6a365a48e0ff
Hamid-Usman <abdulhamidusman218@gmail.com>
2025-06-29 02:45:48 +0100

    adjusted motion effect

--- motion/variants/slideRight.ts
+++ motion/variants/slideRight.ts
@@ -2,6 +2,6 @@ export const  slideRight =  {
     initial: { x: -200, opacity: 0 },
     animate: { x: 0, opacity: 1,},
     exit: { x: -200, opacity: 0 },
-    transition: { duration: 1, ease: "easeOut" }
+    transition: { duration: 1}
     
 }
